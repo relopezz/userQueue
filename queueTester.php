a9236f923ac77d05a3b8f17f9e7ef919ab0ac2bb
Rita Lopez Diaz <rita.lopezdiaz@kaplan.edu>
2015-11-09 23:41:14 -0500

    WIP

--- queueTester.php
+++ queueTester.php
@@ -1,28 +1,35 @@
 <?php
+include 'class.Queue.inc'
 
-include 'class.Queue.inc';
 $users = new userQueue();
 
 //Adding 1 to 6 users
+
 for($i = 1; $i <= 6; $i++) {
     $users->add($i);
 }
 echo '<pre> ADDING USERS'; print_r($users);
 
 //Remove user
-$user = $users->removeUsers(2);
+$user = $users->removeByUser(2);
 echo '<pre> REMOVE USER 2'; print_r($users);
 
-//Remove user
-$user = $users->removeUsers(6);
-echo '<pre> REMOVE USER 6'; print_r($users);
-
 //Remove by Position
-//$user = $users->removeByPosition(1);
-//echo '<pre> REMOVE POSITION 1 -- !'; print_r($users);
+$user = $users->removeByPosition(1);
+echo '<pre> REMOVE POSITION 1 --   '; print_r($users);
 
-//MOVE 3,1 Guessing is move user 3 to the user 1 position
+//MOVE
+$user = $users->move(0,2);
+echo '<pre> MOVE POSITIONs 0,2 --   '; print_r($users);
+
+//SWAP 3,1 Guessing is move user 3 to the user 1 position
+$user = $users->swap(0,2);
+echo '<pre> MOVE POSITIONs 0,2 --   '; print_r($users);
 
 //REVERSE
-$users_reverse = $users->reverseQ();
+$users_reverse = $users->reverse();
 echo '<pre> REVERSE QUEUE'; print_r($users_reverse);
+
+//PRINT
+//$users_reverse = $users->printQueue();
+//echo '<pre> REVERSE QUEUE'; $users_reverse;
